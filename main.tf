10e5204a2c669654bea1384b3cefd75a9e87f4c4
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-12-22 23:34:53 +0000

    added jx file

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
 module "jx" {
   source  = "jenkins-x/jx/google"
   version = "1.11.2"
-  gcp_project = "jenkins-jx-372416"
-  cluster_name = "jsx-cluster"
+  gcp_project = "jenkins-x-372422"
+  cluster_name = "jenkin-cluster"
 }
+output "jx_requirements" {
+  value = module.jx.jx_requirements
+} 
 terraform {
   backend "gcs"{
-    bucket = "jenkins-jx-store-files"
+    bucket = "jenkins-jstore"
     prefix = "teraform/state"
   }
 }
